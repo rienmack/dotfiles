a98f809de22ab81139b80050260224cbf374ea78
Ryan MacKenzie <rienmack@gmail.com>
2024-09-15 06:21:49 -0400

    Add fugitive for git ui

---
+++ nvim/lua/cfg/plugins/fugitive.lua
@@ -0,0 +1,21 @@
+return {
+  'tpope/vim-fugitive',
+  dependencies = {
+    'tpope/vim-rhubarb',
+  },
+  lazy = false,
+  keys = {
+    { '<Leader>gs', ':Gedit :<CR>' },
+    { '<Leader>gb', ':GBrowse<CR>' },
+    { '<Leader>gb', ":'<,'>GBrowse<CR>", mode = 'v' },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd('Filetype', {
+      pattern = 'fugitive',
+      group = vim.api.nvim_create_augroup('fugitive_open', { clear = true }),
+      callback = function()
+        vim.cmd.normal 'gg]]'
+      end,
+    })
+  end,
+}
