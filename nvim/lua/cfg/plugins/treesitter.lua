7de407e8515d3d7089de471debc64e26f36b5746
Ryan MacKenzie <rienmack@gmail.com>
2024-09-19 10:18:35 -0400

    blade support

--- nvim/lua/cfg/plugins/treesitter.lua
+++ nvim/lua/cfg/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return { -- Highlight, edit, and navigate code
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'php', 'javascript' },
+    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'php', 'javascript', 'blade' },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
@@ -15,6 +15,25 @@ return { -- Highlight, edit, and navigate code
     },
     indent = { enable = true },
   },
+  config = function(_, opts)
+    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+
+    parser_config.blade = {
+      install_info = {
+        url = 'https://github.com/EmranMR/tree-sitter-blade',
+        files = { 'src/parser.c' },
+        branch = 'main',
+      },
+      filetype = 'blade',
+    }
+    vim.filetype.add {
+      pattern = {
+        ['.*%.blade%.php'] = 'blade',
+      },
+    }
+
+    require('nvim-treesitter.configs').setup(opts)
+  end,
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
   --
