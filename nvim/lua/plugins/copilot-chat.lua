0c03a10122d060a44a6fae0b8fce0116cd880e13
Ryan MacKenzie <rienmack@gmail.com>
2024-12-02 14:08:23 -0500

    update snacks, copilot chat and add autopairs back

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,14 +1,13 @@
 return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'canary',
     dependencies = {
       { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+      { 'nvim-lua/plenary.nvim' },  -- for curl, log wrapper
     },
-    build = 'make tiktoken', -- Only on MacOS or Linux
+    build = 'make tiktoken',        -- Only on MacOS or Linux
     opts = {
-      debug = true, -- Enable debugging
+      debug = true,                 -- Enable debugging
       -- See Configuration section for rest
     },
   },
