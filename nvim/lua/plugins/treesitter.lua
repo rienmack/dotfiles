8c9354d70bcc25bf702886562cf74cadf6a951fc
Ryan MacKenzie <rienmack@gmail.com>
2025-08-16 07:48:54 -0400

    treesitter updates and add markview

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return { -- Highlight, edit, and navigate code
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
-    ensure_installed = { 'php', 'javascript', 'html', 'typescript', 'lua', 'vim' },
+    ensure_installed = { 'php', 'javascript', 'html', 'typescript', 'lua', 'vim', 'blade', 'javascript' },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
